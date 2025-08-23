a77b9cae10c4be50ef566d723f02602de6a75aaf
Sungmin Park <dev.chris.kr@gmail.com>
2025-08-23 09:08:47 +0100

    Updae Vercel analysis

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 
 	let { children } = $props();
+	
+	injectAnalytics();
 </script>
 
 <svelte:head>
